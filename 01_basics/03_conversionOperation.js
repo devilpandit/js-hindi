49c2e83ecbaa97b84cab7b38eb6c5798f18a0147
Chandra Prakash Sharma <110483370+devilpandit@users.noreply.github.com>
2023-09-18 07:37:15 +0000

    Conversion Operation Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,6 +13,32 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// ***************** Operations************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "prakash"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2");
+
+let x = 3;
+const y = x++;
+
+// console.log(x);
+// console.log(y)
 
 
+let a = 3;
+const b = ++a;
+console.log(a);
+console.log(b)

3fbabacde881f88c8c9b0ea3294d1b163a235262
Chandra Prakash Sharma <110483370+devilpandit@users.noreply.github.com>
2023-09-18 06:25:46 +0000

    Datatypes Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+//alert(3 + 3) // we are using node js not browser
+
+let name = "prakash"
+let age = 18
+let isLoggedIn = false
+let state; // undefined varaible
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+
+
+
+
+
